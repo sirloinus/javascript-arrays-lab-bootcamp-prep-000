6b830ca4ae050b2e254afee667c834404f2cf68a
Amalie S <amaliesmidth@gmail.com>
2018-05-31 07:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function kittens () {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+ function destructivelyAppendKitten(name) {
+   kittens.push(name)
+   return kittens
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop()
+  return
+}
+
