df2c85586db0bc64b6f825e81234185495915a67
Amalie S <amaliesmidth@gmail.com>
2018-05-31 15:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,29 +36,10 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
+  const newKittens = kittens.slice(-1)
+  return newKittens
+}
   
-}
-
 function removeFirstKitten() {
-
+  const newKittens = kittens.slice(1)
 }
-
-
-
-  describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-  describe('removeFirstKitten()', function() {
-    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-})
